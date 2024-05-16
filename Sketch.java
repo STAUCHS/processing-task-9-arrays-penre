9f03f457ff0b815408761495908d3f4c69ffac57
penre <tianyou.zhang25@ycdsbk12.ca>
2024-05-16 14:29:46 -0400

    finished

--- Sketch.java
+++ Sketch.java
@@ -4,6 +4,8 @@ public class Sketch extends PApplet {
 
   float [] snowX = new float [30];
   float [] snowY = new float [30];  
+  boolean [] ballHideStatus = new boolean [30];
+
   int snowDiameter = 20;
   int playerSize = 20;
   float circleX = 200;
@@ -18,46 +20,34 @@ public class Sketch extends PApplet {
 
   public void setup() {
     background(0);
-
-    //generate random x and y values for snowflakes
     for (int i = 0; i < snowX.length; i++){
+      //generate random x and y values for snowflakes
       snowX[i] = random(width);
       snowY[i] = random(height);
+      ballHideStatus[i] = false;
     }
   }
     
   public void draw() {
+
+    //if lives is 0 then screen turns white
     if (intLives == 0){
       background(255);
     }
     else {
       background(0);
     
-    //draw snow
-    snow();
+      //draw snow
+      snow();
 
-    //control speed of snow
-    for (int i = 0; i < snowX.length; i++){
-      if (keyCode == UP){
-        snowY[i] -= 1;
+      //lives
+      for (int i = 1; i <= intLives; i++){
+        fill (255, 0, 0);
+        rect(width - i * 50, height - 400, 40, 40);
       }
-      else if (keyCode == DOWN){
-        snowY[i] += 10;
-      }
-      else if (keyCode == RIGHT){
-        snowY[i] += 2;
-      }
-    }
 
-    //lives
-    for (int i = 1; i <= intLives; i++){
-      fill (255, 0, 0);
-      rect(width - i * 50, height - 400, 40, 40);
-    }
-
-    //draw player
-    player();
-    
+      //draw player
+      player();
     }
   }
     
@@ -65,19 +55,37 @@ public class Sketch extends PApplet {
   public void snow(){
     fill(255);
     for (int i = 0; i < snowX.length; i++){
-      circle(snowX[i], snowY[i], snowDiameter);
 
-      snowY[i] += 2;
-
-      //reset snowflakes
-      if (snowY[i] > height){
+       //reset snowflakes
+       if (snowY[i] > height){
         snowY[i] = -10;
       }
+      if(!ballHideStatus[i]){
+       circle(snowX[i], snowY[i], snowDiameter);
 
-      //collision FINISH LIVES THINGS
-      if (dist(snowX[i], snowY[i], circleX, circleY) < snowDiameter / 2 + playerSize / 2 ){
-        snowY[i] = 0;
-        intLives --;
+        //collision with player
+        if (dist(snowX[i], snowY[i], circleX, circleY) < snowDiameter / 2 + playerSize / 2 ){
+          snowY[i] = 0;
+          intLives --;
+          if (intLives <=0) {
+          }
+        }
+        // mouse clicks
+        if (mousePressed){
+          if(dist(mouseX, mouseY, snowX[i], snowY[i]) < snowDiameter){
+            ballHideStatus[i] = true;
+          }
+        }
+        //controls speed of snow
+        if (keyCode == UP){
+          snowY[i] -= 1;
+        } 
+        else if (keyCode == DOWN){
+          snowY[i] += 10;
+        }
+        else{
+          snowY[i] += 2;
+        }
       }
     }
   }
